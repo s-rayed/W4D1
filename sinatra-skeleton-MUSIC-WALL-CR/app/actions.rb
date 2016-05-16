f81ad4342f9b5ebe4a7e31a5cf5adcd35aba2e2c
Rayed <siddiqui.rayed@gmail.com>
2016-05-16 12:08:13 -0400

    user can now add songs to music wall

--- sinatra-skeleton-MUSIC-WALL-CR/app/actions.rb
+++ sinatra-skeleton-MUSIC-WALL-CR/app/actions.rb
@@ -7,3 +7,21 @@ get '/songs' do
   @songs = Song.all
   erb :'songs/index'
 end
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    artist: params[:artist],
+    title: params[:title],
+    url: params[:url]
+    )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
